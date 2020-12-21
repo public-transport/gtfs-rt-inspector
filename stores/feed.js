6f357ac76191e5eb96e30550e679f628d584b9a2
Jannis R <mail@jannisr.de>
2020-12-21 03:13:08 +0100

    receive & parse GTFS-RT

--- stores/feed.js
+++ stores/feed.js
@@ -1,10 +1,48 @@
 import {debounce} from 'lodash'
+import {parse as parseContentType} from 'content-type'
+const {Buffer} = require('buffer/') // trailing slash is intentional
+const {FeedMessage} = require('gtfs-rt-bindings')
 import * as syncViaPeriodicFetch from 'fetch-periodic-sync'
+
+const MAX_FEED_SIZE = 5 * 1024 * 1024 // 5mb
 
 const feedStore = (state, bus) => {
 	state.feedUrl = null
+	state.feedSyncStopped = false
 	state.feedSyncing = false
+	state.feedRawData = null
+	state.feedData = null
 	let sync = null
+
+	const receiveAndParseFeed = async (res) => {
+		const cTypeHeader = res.headers.get('content-type')
+		const cType = cTypeHeader ? parseContentType(cTypeHeader) : {}
+		if (cType.type && cType.type !== 'application/octet-stream') {
+			const err = new Error('invalid content-type, only application/octet-stream is supported')
+			err.response = res
+			throw err
+		}
+
+		const size = res.headers.get('content-length')
+		if (size && parseInt(size) > MAX_FEED_SIZE) {
+			const err = new Error('response is too large')
+			err.response = res
+			throw err
+		}
+
+		const buf = Buffer.from(await res.arrayBuffer())
+		const data = FeedMessage.decode(buf)
+		if (!data || !data.header || !Array.isArray(data.entity)) {
+			const err = new Error(`couldn't parse feed`)
+			err.rawFeed = buf
+			err.response = res
+			throw err
+		}
+
+		state.feedRawData = buf
+		state.feedData = data
+		bus.emit('render')
+	}
 
 	const setSyncing = (syncing) => {
 		state.feedSyncing = syncing
@@ -16,6 +54,11 @@ const feedStore = (state, bus) => {
 		})
 		sync.on('fetch', () => setSyncing(true))
 		sync.on('fetch-done', () => setSyncing(false))
+		sync.on('change', (res) => {
+			receiveAndParseFeed(res)
+			// todo: handle errors properly
+			.catch(console.error)
+		})
 		// todo: error
 	}, 100)
 
@@ -38,6 +81,18 @@ const feedStore = (state, bus) => {
 	bus.on('feed:sync', () => {
 		if (!sync) return;
 		sync.refetch()
+	})
+	bus.on('feed:stop-sync', () => {
+		if (!sync || !sync.isActive()) return;
+		sync.stop()
+		state.feedSyncStopped = true
+		bus.emit('render')
+	})
+	bus.on('feed:start-sync', () => {
+		if (!sync || sync.isActive()) return;
+		sync.start()
+		state.feedSyncStopped = false
+		bus.emit('render')
 	})
 }
 
--- views/main.js
+++ views/main.js
@@ -7,12 +7,17 @@ const mainView = (state, emit) => {
 	const handleSyncClick = () => {
 		emit('feed:sync')
 	}
+	const handleStopStartClick = () => {
+		if (state.feedSyncStopped) emit('feed:start-sync')
+		else emit('feed:stop-sync')
+	}
 
 	return (
 		<div>
 			<p>{state.feedUrl}</p>
 			<input onInput={handleFeedUrlInput} value={state.feedUrl} />
 			<button onClick={handleSyncClick} disabled={!!state.feedSyncing}>↺</button>
+			<button onClick={handleStopStartClick}>{state.feedSyncStopped ? '▶️' : '⏸'}</button>
 		</div>
 	)
 }
